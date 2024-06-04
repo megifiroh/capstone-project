2d86d1ad47b1c613d3d2c294af0b5245d958b358
Kholifah Magfiroh <kholifah.magfiroh501@gmail.com>
2024-06-05 02:49:48 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+const app = express();
+
+// Connect to database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
